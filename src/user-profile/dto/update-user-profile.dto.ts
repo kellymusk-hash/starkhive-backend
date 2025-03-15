13faad1482ca948c95e89ff0846f132465ee8c4a
shamoo53 <aliyushamsu16@gmail.com>
2025-03-15 17:10:37 +0100

    made a change to the update Dto

--- src/user-profile/dto/update-user-profile.dto.ts
+++ src/user-profile/dto/update-user-profile.dto.ts
@@ -1,15 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserProfileDto } from './create-user-profile.dto';
-import {
-  IsEmail,
-  IsNotEmpty,
-  IsOptional,
-  IsString,
-  IsBoolean,
-  IsNumber,
-  IsArray,
-  IsIn,
-} from 'class-validator';
+
 
 export class UpdateUserProfileDto extends PartialType(CreateUserProfileDto) {
 }
