02782c0f1673ff7ec09de0b1022365fc15b223f2
mdauwal <pricelezzchap@gmail.com>
2025-03-17 13:21:41 +0000

    exported:  Repository and Service arr now properly connected

--- src/freelancer-profile/freelancer-profile.service.ts
+++ src/freelancer-profile/freelancer-profile.service.ts
@@ -1,26 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { FreelancerProfileRepository } from './repositories/freelancer-profile.repository';
 import { CreateFreelancerProfileDto } from './dto/create-freelancer-profile.dto';
-import { UpdateFreelancerProfileDto } from './dto/update-freelancer-profile.dto';
 
 @Injectable()
 export class FreelancerProfileService {
-  create(createFreelancerProfileDto: CreateFreelancerProfileDto) {
-    return 'This action adds a new freelancerProfile';
+  constructor(private readonly freelancerProfileRepository: FreelancerProfileRepository) {}
+
+  async createProfile(payload: CreateFreelancerProfileDto) {
+    return this.freelancerProfileRepository.createFreelancerProfile(payload);
   }
 
-  findAll() {
-    return `This action returns all freelancerProfile`;
+  async getProfileByUserId(userId: string) {
+    return this.freelancerProfileRepository.findProfileByUserId(userId);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} freelancerProfile`;
+  async updateSkills(id: string, skills: string[]) {
+    return this.freelancerProfileRepository.updateSkills(id, skills);
   }
 
-  update(id: number, updateFreelancerProfileDto: UpdateFreelancerProfileDto) {
-    return `This action updates a #${id} freelancerProfile`;
+  async updateExperience(id: string, experience: string) {
+    return this.freelancerProfileRepository.updateExperience(id, experience);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} freelancerProfile`;
+  async deleteProfile(id: string) {
+    return this.freelancerProfileRepository.deleteProfile(id);
   }
 }
